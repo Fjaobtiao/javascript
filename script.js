cccda1edaf5d159ec9779f177d0797fa7ea19f69
Fjaobtiao <jotafn@icloud.com>
2021-08-10 16:00:31 -0300

    enviaragora

---
+++ script.js
@@ -0,0 +1,54 @@
+// function out(x) {
+     
+//     document.write("Seu nome é:" , x)
+// }
+
+
+// FUNCÃO PUSH
+// function cadastrar_funcionarios(){
+//     var funcionarios = [];
+//     while(true){
+//         aux = prompt("Digite o nome do próximo funcionário ou 0 para sair");
+//         if(aux == 0)
+//             break;
+//         else
+//             funcionarios.push(aux);
+//     }
+
+//     for (i in funcionarios){
+//         document.write("<br>o nome é: ", funcionarios[i]);
+//     }
+// }
+
+// FUNCÃO UNSHIFT coloca o último no primeiro
+// function cadastrar_funcionarios(){
+//     var funcionarios = [];
+//     while(true){
+//         aux = prompt("Digite o nome do próximo funcionário ou 0 para sair");
+//         if(aux == 0)
+//             break;
+//         else
+//             funcionarios.unshift(aux);
+//     }
+
+//     for (i in funcionarios){
+//         document.write("<br>o nome é: ", funcionarios[i]);
+//     }
+// }
+
+// FUNCÃO POP apaga uma posição dos itens armazenados. No caso do exemplo ele esta apagando a posição 4
+// function cadastrar_funcionarios(){
+//     var num = [1,2,3,4,5,6,7,8,9,10];
+
+//     for(i=0;i<num.length();i++){
+//         if(i>3)
+//             num[i] = num[parseInt(i) + 1]
+//     }
+    
+//     num.pop();
+
+//      for(i in num){
+//         document.write("<br>num: ", num[i]);
+//     }
+// }
+
