8f61068e00550b1c8e8b3f5652e7c2f7d8b9798d
Fjaobtiao <jotafn@icloud.com>
2021-08-10 17:38:02 -0300

    enviaragora

--- script.js
+++ script.js
@@ -52,3 +52,42 @@
 //     }
 // }
 
+
+
+// var alunos_nota=[];
+// // nome, nota html, nota css, nota javascript
+// alunos_nota['nome'] = [];
+// alunos_nota['nota_html'] = [];
+// alunos_nota['nota_css'] = [];
+// alunos_nota['nota_javascript'] = [];
+// alunos_nota['media'] = []
+
+// alunos_nota['nome'][0] = "Joao";
+// alunos_nota['nota_html'][0] = 70;
+// alunos_nota['nota_css'][0] = 66;
+// alunos_nota['nota_javascript'][0] = 88;
+
+// alunos_nota['nome'][1] = "Maria";
+// alunos_nota['nota_html'][1] = 70;
+// alunos_nota['nota_css'][1] = 60;
+// alunos_nota['nota_javascript'][1] = 60;
+
+// alunos_nota['nome'][2] = "Cesar";
+// alunos_nota['nota_html'][2] = 80;
+// alunos_nota['nota_css'][2] = 80;
+// alunos_nota['nota_javascript'][2] = 80;
+
+// for(i=0;i<alunos_nota['nome'].length;i++){
+//     alunos_nota['media'][i] = (alunos_nota['nota_html'][i]+alunos_nota['nota_css'][i]+alunos_nota['nota_javascript'][i])/3
+// }
+
+
+// for(i=0;i<alunos_nota['nome'].length;i++){
+//     document.write(alunos_nota['nome'][i], " ", alunos_nota['nota_html'][i], " ", alunos_nota['nota_css'][i], " ", alunos_nota['nota_javascript'][i], " ", alunos_nota['media'][i], "<br>")
+// }
+
+
+// inverter a posição das strings
+var teste = ["F","r",2,3,5,2,6,"J","a","T",1,2,4];
+teste.reverse();
+document.write(teste)
